7b20561e07f06fabaf5f3a579a50edc68cbc7700
zhouyq <zyq916@gmail.com>
2016-04-13 00:08:32 +0800

    gosu plugin install

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,7 +46,7 @@ fi
 # install discovery-multicast plugin
 installed=`/elasticsearch/bin/plugin list | grep discovery-multicast`
 [ ! "$installed" ] && cp /tmp/tmp_elasticsearch.yml /elasticsearch/config/ && \
-/elasticsearch/bin/plugin install discovery-multicast
+gosu rain /elasticsearch/bin/plugin install discovery-multicast
 
 # 处理 elasticsearch 配置文件
 cp /tmp/${ESCONFIG}  ${CONFDIR}/${POD_ORDER}/${ESCONFIG}

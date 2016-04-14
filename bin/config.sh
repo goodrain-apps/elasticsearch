56c2127b89f21e69d417dd62a6c5bf8ac1a8fb3f
zhouyq <zyq916@gmail.com>
2016-04-14 10:45:36 +0800

    Update config.sh

--- bin/config.sh
+++ bin/config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-NODE_LIST=$1
+NODE_LIST=$(echo $1 | sed  's/,/","/g')
 
-CLUSTER_LIST="[${NODE_LIST}]"
+CLUSTER_LIST="[\"${NODE_LIST}\"]"
 
 sed -i -r "s/#(discovery.zen.ping.unicast.hosts:) .*/\1 $CLUSTER_LIST/" /elasticsearch/config/elasticsearch.yml && \
 echo "Elasticsearch config updated successfully."

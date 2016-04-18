c850cf645224df502fe39f6a19fa6a53569d132b
zhouyq <zyq916@gmail.com>
2016-04-18 17:34:02 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -79,8 +79,8 @@ if [ "$MULTICAST" != "true" ];then
        -rec_cmd=/elasticsearch/bin/config.sh
 fi
 
-sed -i -r "s/(http.bind_host:) .*/\1 [$MULIT_IP]/" ${CONFDIR}/${POD_ORDER}/${ESCONFIG}
-sed -i -r "s/(transport.bind_host:) .*/\1 $HOST_IP/" ${CONFDIR}/${POD_ORDER}/${ESCONFIG}
+sed -i -r "s/(network.host:) .*/\1 [$MULIT_IP]/" ${CONFDIR}/${POD_ORDER}/${ESCONFIG}
+#sed -i -r "s/(transport.bind_host:) .*/\1 $HOST_IP/" ${CONFDIR}/${POD_ORDER}/${ESCONFIG}
 
 # Add elasticsearch as command if needed
 if [ "${1:0:1}" = '-' ]; then
